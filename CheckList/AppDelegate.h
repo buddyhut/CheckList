804ed464c9a10de57d342f331785dc67c3fc4e0d
Alok jha <contact@aloks-mac-mini.local.lan>
2013-07-01 13:48:07 +0530

    Initial Commit

---
+++ CheckList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckList
+//
+//  Created by Alok jha on 7/1/13.
+//  Copyright (c) 2013 com.futeroxaapcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CheckList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CheckList
+//
+//  Created by Alok jha on 7/1/13.
+//  Copyright (c) 2013 com.futeroxaapcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
